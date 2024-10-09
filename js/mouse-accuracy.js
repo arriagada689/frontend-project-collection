3c44fe0b4b354675ffa396dd9ca7bb4a63ca1e18
Diego Arriagada <arriagada689@gmail.com>
2024-10-09 19:28:11 -0400

    dark mode fix

--- js/mouse-accuracy.js
+++ js/mouse-accuracy.js
@@ -319,37 +319,3 @@ window.addEventListener('load', () => {
 endSessionBtn.addEventListener('click', () => {
     handleEndGame()
 })
-
-const darkModeToggle = document.getElementById('dark-mode-toggle')
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-
-    //add sun icon
-    const sunIcon = document.createElement('i')
-    sunIcon.className = 'fa-solid fa-sun text-2xl'
-    darkModeToggle.appendChild(sunIcon)
-} else {
-    document.documentElement.classList.remove('dark')
-    const moonIcon = document.createElement('i')
-    moonIcon.className = 'fa-solid fa-moon text-2xl'
-    darkModeToggle.appendChild(moonIcon)
-}
-
-darkModeToggle.addEventListener('click', () => {
-    const isDarkMode = document.documentElement.classList.contains('dark');
-    if(isDarkMode){
-        document.documentElement.classList.remove('dark');
-        localStorage.theme = 'light';
-        darkModeToggle.innerHTML = ''; 
-        const moonIcon = document.createElement('i');
-        moonIcon.className = 'fa-solid fa-moon text-2xl';
-        darkModeToggle.appendChild(moonIcon);
-    } else {
-        document.documentElement.classList.add('dark');
-        localStorage.theme = 'dark';
-        darkModeToggle.innerHTML = '';
-        const sunIcon = document.createElement('i');
-        sunIcon.className = 'fa-solid fa-sun text-2xl';
-        darkModeToggle.appendChild(sunIcon);
-    }
-})
