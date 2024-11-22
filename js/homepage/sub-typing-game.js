a198689f3dedc18f377ba236d2cbfe4a988e8739
Diego Arriagada <arriagada689@gmail.com>
2024-11-22 17:01:01 -0500

    keyboard animation functionality

--- js/homepage/sub-typing-game.js
+++ js/homepage/sub-typing-game.js
@@ -2,34 +2,72 @@
 
 const inputBox = document.getElementById('input-box');
 const words = document.getElementById('words');
+const keyboard = document.getElementById('keyboard');
 
 let wordStr = 'Hello and welcome to the typing game!'
 let index = 0
 let wordsArray
 let currentWordIndex = 0
 let tempIndex = 0
+let keyboardArr = [' ', '1 !', '', '', '', '', '', '', '', '', '', '', '', 'empty',
+                   'empty', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '', '', '',
+                   'empty', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '', '', 'empty',
+                   'triple', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '', '', '', 'empty', 
+                   'em', 'em', 'em', 'em', 'Space']
+
+createWordElements()
+createKeyboard()
 
 let animationInterval = setInterval(() => {
 
+    //get rid of green bg color
+    Array.from(keyboard.children).forEach(key => {
+        if(key.innerText || key.id === 'space'){
+            key.classList.remove('bg-green-500')
+            key.classList.add('bg-white')
+        }
+    })
+    
     if(tempIndex === words.children[currentWordIndex].innerText.length){
-        inputBox.value = ' '
+        inputBox.innerText = ' '
         currentWordIndex += 2
         tempIndex = 0
+
+        //space button bg color
+        const spaceKey = document.getElementById('space')
+        spaceKey.classList.remove('bg-white')
+        spaceKey.classList.add('bg-green-500')
+
     } else {
-        //handle reset
-        if(index === wordStr.length - 1){
-            handleAnimationReset()
-        }
-
-        inputBox.value += wordStr[index]
+        
+        inputBox.innerText += wordStr[index]
         words.children[currentWordIndex].children[tempIndex].classList.add('text-green-500')
-        tempIndex++
-    }
-    index++
-}, 150)
 
-createWordElements()
-// console.log(words.children);
+        //iterate through keyboard divs and match with clicked key
+        Array.from(keyboard.children).forEach(key => {
+            if(key.textContent === wordStr[index].toUpperCase()){
+                
+                key.classList.remove('bg-white')
+                key.classList.add('bg-green-500')
+            } else if(wordStr[index] === '!'){
+                const oneKey = document.getElementById('one')
+                oneKey.classList.remove('bg-white')
+                oneKey.classList.add('bg-green-500')
+            }
+        })
+
+        tempIndex++
+
+    }
+    
+    //handle reset
+    if(index === wordStr.length){
+        handleAnimationReset()
+    } else {
+        index++
+    }
+    
+}, 180)
 
 function handleAnimationReset(){
     //make all letters normal color
@@ -44,7 +82,7 @@ function handleAnimationReset(){
     tempIndex = 0
 
     //wipe input box
-    inputBox.value = ' '
+    inputBox.innerText = ' '
 }
 
 function createWordElements(){
@@ -71,6 +109,39 @@ function createWordElements(){
             words.appendChild(wordElement); 
             words.appendChild(spaceElement); 
         }
+    })
+}
+
+function createKeyboard() {
+    
+    keyboardArr.forEach(key => {
+        const keyDiv = document.createElement('div')
+        keyDiv.textContent = key === 'empty' || key === 'em' || key === 'triple' ? '' : key
+        keyDiv.className = 'bg-white p-2 text-center rounded ';
+
+        //set col span for big keys
+        if(key === 'back' || key === 'Tab' || key === 'Caps' || key === 'Enter' || key === 'empty'){
+            keyDiv.classList.add('col-span-2')
+        } else if(key === 'triple'){
+            keyDiv.classList.add('col-span-3')
+        } else if(key === 'Space'){
+            keyDiv.textContent = ''
+            keyDiv.classList.add('col-span-7')
+            keyDiv.id = 'space'
+        }
+
+        //empty keys
+        if(key === 'em'){
+            keyDiv.classList.remove('bg-white')
+            keyDiv.classList.add('bg-gray-300')
+        }
+
+        //add id to 1 key
+        if(key === '1 !'){
+            keyDiv.id = 'one'
+        }
+
+        keyboard.appendChild(keyDiv);
     })
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
       colors: {
         newGame: '#3cc1a3',
         tryAgain: '#ffd71f'
-      }
+      },
+      gridColumn: {
+        'span-5': 'span 5 / span 5',  // Custom span for Space key
+      },
     },
   },
   plugins: [],
